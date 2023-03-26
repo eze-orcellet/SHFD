8933b60960c32dc3e70fdb19e4fcbd7e5efea71e
Ezequiel Orcellet <99030748+eze-orcellet@users.noreply.github.com>
2023-03-25 21:49:09 -0300

    Update fs.py

--- fs.py
+++ fs.py
@@ -1,7 +1,8 @@
-def __init__(self,selected_features):
-    self.selected_features=selected_features
+class FeatureSelection(BaseEstimator, TransformerMixin):
+    def __init__(self,selected_features):
+        self.selected_features=selected_features
     
-def fit(self,X,y=None):
-    return self
-def transform(self, X, y=None):
-    return X[self.selected_features]
+    def fit(self,X,y=None):
+        return self
+    def transform(self, X, y=None):
+        return X[self.selected_features]
