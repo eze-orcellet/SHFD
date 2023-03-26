5330c803fb7ccedbce8f209da97a9355f3bc0559
Ezequiel Orcellet <99030748+eze-orcellet@users.noreply.github.com>
2023-03-25 21:21:52 -0300

    Update fs.py

--- fs.py
+++ fs.py
@@ -1,12 +1,7 @@
-
-from sklearn.base import BaseEstimator, TransformerMixin
-class FeatureSelection(BaseEstimator, TransformerMixin):
-
-    def __init__(self,selected_features):
-        self.selected_features=selected_features
+def __init__(self,selected_features):
+    self.selected_features=selected_features
     
-    def fit(self,X,y=None):
-        return self
-
-    def transform(self, X, y=None):
-        return X[self.selected_features]
+def fit(self,X,y=None):
+    return self
+def transform(self, X, y=None):
+    return X[self.selected_features]
